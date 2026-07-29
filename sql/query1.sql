a0a1e65bfb6640cd997920276539b8ba8c6d624a
Pavel Krupar <pavel.krupar@gmail.com>
2026-07-29 12:49:10 +0200

    query1

--- sql/query1.sql
+++ sql/query1.sql
@@ -1,1 +1,23 @@
--- empty file
+
+-- Show 10 top-paying remote jobs for Data Analyst role
+
+
+SELECT
+    job_title_short,
+    job_title,
+    job_location,
+    job_schedule_type,
+    company_dim.name AS company_name,
+    salary_year_avg,
+    job_posted_date
+FROM 
+    job_postings_fact
+LEFT JOIN 
+    company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE 
+    job_title_short = 'Data Analyst' AND 
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY 
+    salary_year_avg DESC
+LIMIT 10;
